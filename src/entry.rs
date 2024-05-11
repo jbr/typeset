d5b9bd8e9d3cb3f35baaf07adfa7bcdd87fe788f
Jacob Rothstein <hi@jbr.me>
2024-05-10 19:11:03 -0700

    feat: 📰 initial commit

---
+++ src/entry.rs
@@ -0,0 +1,231 @@
+use crate::{unwrap, Key, Value};
+use std::{
+    any::{type_name, TypeId},
+    collections::btree_map,
+    fmt::{self, Formatter},
+    marker::PhantomData,
+    ops::{Deref, DerefMut},
+};
+
+/// A view into a single type in the `TypeSet`, which may be either vacant or occupied.
+///
+/// This type is constructed by [`TypeSet::entry`][crate::TypeSet::entry]
+///
+/// ## Examples
+///
+/// This is a somewhat contrived example that demonstrates matching on the [`Entry`]. Often,
+/// [`Entry::or_insert`], [`Entry::or_insert_with`], and [`Entry::and_modify`] can achieve
+/// comparable results. See those functions for further usage examples.
+///
+/// ```rust
+/// use typeset::{TypeSet, entry::Entry};
+/// let mut set = TypeSet::new().with("hello");
+/// let (previous, current) = match set.entry::<&'static str>() {
+///     Entry::Vacant(vacant_entry) => {
+///         let current = vacant_entry.insert("entry was vacant");
+///         (None, current)
+///     }
+///
+///     Entry::Occupied(mut occupied_entry) => {
+///         let previous = occupied_entry.insert("entry was occupied");
+///         (Some(previous), occupied_entry.into_mut())
+///     }
+/// };
+/// assert_eq!(previous, Some("hello"));
+/// assert_eq!(*current, "entry was occupied");
+/// ```
+#[derive(Debug)]
+pub enum Entry<'a, T> {
+    /// A view into the location a T would be stored in the `TypeSet`. See [`VacantEntry`]
+    Vacant(VacantEntry<'a, T>),
+
+    /// A view into the location a T is currently stored in the `TypeSet`. See [`OccupiedEntry`]
+    Occupied(OccupiedEntry<'a, T>),
+}
+
+/// A view into a vacant entry in a `TypeSet`.
+///
+/// It is part of the [`Entry`] enum.
+pub struct VacantEntry<'a, T>(
+    pub(super) btree_map::VacantEntry<'a, Key, Value>,
+    PhantomData<T>,
+);
+
+impl<'a, T> fmt::Debug for VacantEntry<'a, T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("VacantEntry")
+            .field(&type_name::<T>())
+            .finish()
+    }
+}
+/// A view into the location a T is stored
+pub struct OccupiedEntry<'a, T>(
+    pub(super) btree_map::OccupiedEntry<'a, Key, Value>,
+    PhantomData<T>,
+);
+
+impl<'a, T: fmt::Debug> fmt::Debug for OccupiedEntry<'a, T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("OccupiedEntry").field(self.0.get()).finish()
+    }
+}
+
+impl<'a, T: Send + Sync + 'static> Entry<'a, T> {
+    /// Ensures a value is in the `Entry` by inserting the provided `default` value if the Entry was
+    /// previously vacant. Returns a mutable reference to the value.
+    ///
+    /// Prefer [`Entry::or_insert_with`] if constructing a T is expensive.
+    ///
+    /// ## Example
+    ///
+    /// ```rust
+    /// let mut set = typeset::TypeSet::new();
+    /// assert_eq!(*set.entry().or_insert("hello"), "hello");
+    /// assert_eq!(set.get::<&'static str>(), Some(&"hello"));
+    /// assert_eq!(*set.entry().or_insert("world"), "hello");
+    /// assert_eq!(set.get::<&'static str>(), Some(&"hello"));
+    /// ```
+    pub fn or_insert(self, default: T) -> &'a mut T {
+        match self {
+            Entry::Vacant(vacant) => vacant.insert(default),
+            Entry::Occupied(occupied) => occupied.into_mut(),
+        }
+    }
+
+    /// Ensures a value is in the `Entry` by inserting the provided value returned by the `default`
+    /// function if the `Entry` was previously vacant. Returns a mutable reference to the value.
+    ///
+    /// Prefer this to [`Entry::or_insert`] if constructing a T is expensive.
+    ///
+    /// ## Example
+    ///
+    /// ```rust
+    /// let mut set = typeset::TypeSet::new();
+    /// assert_eq!(*set.entry().or_insert_with(|| String::from("hello")), "hello");
+    /// assert_eq!(set.get::<String>(), Some(&String::from("hello")));
+    /// assert_eq!(*set.entry::<String>().or_insert_with(|| panic!("never called")), "hello");
+    /// assert_eq!(set.get::<String>(), Some(&String::from("hello")));
+    /// ```
+    pub fn or_insert_with(self, default: impl FnOnce() -> T) -> &'a mut T {
+        match self {
+            Entry::Vacant(vacant) => vacant.insert(default()),
+            Entry::Occupied(occupied) => occupied.into_mut(),
+        }
+    }
+
+    /// Provides in-place mutable access to an occupied entry before any potential inserts into the
+    /// set using [`Entry::or_insert`] or [`Entry::or_insert_with`].
+    ///
+    /// ## Example
+    ///
+    /// ```rust
+    /// let mut set = typeset::TypeSet::new().with(String::from("hello"));
+    /// let value = set.entry::<String>()
+    ///     .and_modify(|s| s.push_str(" world"))
+    ///     .or_insert_with(|| String::from("greetings"));
+    /// assert_eq!(value, "hello world");
+    ///
+    /// set.remove::<String>();
+    /// let value = set.entry::<String>()
+    ///     .and_modify(|s| s.push_str(" world"))
+    ///     .or_insert_with(|| String::from("greetings"));
+    /// assert_eq!(value, "greetings");
+    /// ```
+    #[must_use]
+    pub fn and_modify(self, f: impl FnOnce(&mut T)) -> Self {
+        match self {
+            Entry::Vacant(vacant) => Entry::Vacant(vacant),
+            Entry::Occupied(mut occupied) => {
+                f(occupied.get_mut());
+                Entry::Occupied(occupied)
+            }
+        }
+    }
+
+    pub(super) fn new(entry: btree_map::Entry<'a, TypeId, Value>) -> Self {
+        match entry {
+            btree_map::Entry::Vacant(vacant) => Self::Vacant(VacantEntry(vacant, PhantomData)),
+            btree_map::Entry::Occupied(occupied) => {
+                Self::Occupied(OccupiedEntry(occupied, PhantomData))
+            }
+        }
+    }
+}
+
+impl<'a, T: Default + Send + Sync + 'static> Entry<'a, T> {
+    /// Ensures a value is in the Entry by inserting the default value if vacant, and returns a
+    /// mutable reference to the value.
+    ///
+    /// Equivalent to `.or_insert_with(Default::default)`
+    ///
+    /// ## Example
+    ///
+    /// ```rust
+    /// let mut set = typeset::TypeSet::new();
+    /// assert_eq!(*set.entry::<&'static str>().or_default(), "");
+    /// set.insert("hello");
+    /// assert_eq!(*set.entry::<&'static str>().or_default(), "hello");
+    /// ```
+    pub fn or_default(self) -> &'a mut T {
+        #[allow(clippy::unwrap_or_default)]
+        // this is the implementation of or_default so it can't call or_default
+        self.or_insert_with(T::default)
+    }
+}
+
+impl<'a, T: Send + Sync + 'static> VacantEntry<'a, T> {
+    /// Sets the value of this entry to the provided `value`
+    pub fn insert(self, value: T) -> &'a mut T {
+        unwrap!(self.0.insert(Box::new(value)).downcast_mut())
+    }
+}
+
+impl<'a, T: Send + Sync + 'static> OccupiedEntry<'a, T> {
+    /// Gets a reference to the value in this entry
+    #[must_use]
+    pub fn get(&self) -> &T {
+        unwrap!(self.0.get().downcast_ref())
+    }
+
+    /// Gets a mutable reference to the value in the entry
+    ///
+    /// If you need a reference to the `OccupiedEntry` that may outlive the
+    /// destruction of the `Entry` value, see [`OccupiedEntry::into_mut`].
+    #[must_use]
+    pub fn get_mut(&mut self) -> &mut T {
+        unwrap!(self.0.get_mut().downcast_mut())
+    }
+
+    /// Sets the value of the entry to `value`, returning the entry's previous value.
+    pub fn insert(&mut self, value: T) -> T {
+        *unwrap!(self.0.insert(Box::new(value)).downcast())
+    }
+
+    /// Take ownership of the value from this Entry
+    #[allow(clippy::must_use_candidate)] // sometimes we just want to take the value out and drop it
+    pub fn remove(self) -> T {
+        *unwrap!(self.0.remove().downcast())
+    }
+
+    /// Converts the entry into a mutable reference to its value.
+    ///
+    /// If you need multiple references to the `OccupiedEntry`, see [`OccupiedEntry::get_mut`].
+    #[must_use]
+    pub fn into_mut(self) -> &'a mut T {
+        unwrap!(self.0.into_mut().downcast_mut())
+    }
+}
+
+impl<'a, T: Send + Sync + 'static> Deref for OccupiedEntry<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.get()
+    }
+}
+
+impl<'a, T: Send + Sync + 'static> DerefMut for OccupiedEntry<'a, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.get_mut()
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,264 @@
+#![deny(
+    clippy::dbg_macro,
+    missing_copy_implementations,
+    rustdoc::missing_crate_level_docs,
+    missing_debug_implementations,
+    nonstandard_style,
+    unused_qualifications
+)]
+#![warn(missing_docs, clippy::pedantic, clippy::perf, clippy::cargo)]
+#![allow(clippy::missing_panics_doc, clippy::module_name_repetitions)]
+/*!
+
+[`TypeSet`] is a collection for heterogeneous types. Each type can only exist once in the set, and
+can only be retrieved by naming the type.
+
+Because types can only be retrieved by naming them, rust's module system allows module-private
+storage in a shared `TypeSet`.
+
+Currently, this crate imposes `Send + Sync` bounds on the stored types, but future versions may
+offer variants without those bounds and/or with Clone bounds.
+
+Implementation is based on
+- <https://github.com/hyperium/http/blob/master/src/extensions.rs>
+- <https://github.com/kardeiz/type-map/blob/master/src/lib.rs>
+- <https://github.com/http-rs/http-types/blob/main/src/extensions.rs>
+*/
+use std::{
+    any::{Any, TypeId},
+    collections::BTreeMap,
+    fmt::{self, Formatter},
+};
+
+/// Types for interacting with a mutable view into a `TypeSet` for a given type
+pub mod entry;
+use entry::Entry;
+
+type Value = Box<dyn Any + Send + Sync>;
+type Key = TypeId;
+
+macro_rules! unwrap {
+    ($x:expr) => {
+        if cfg!(debug_assertions) {
+            ($x).unwrap()
+        } else {
+            unsafe { ($x).unwrap_unchecked() }
+        }
+    };
+}
+use unwrap;
+
+/// A collection for heterogenous types
+///
+/// Note that there is currently no way to iterate over the collection, as there may be types stored
+/// that cannot be named by the calling code
+#[derive(Default)]
+pub struct TypeSet(BTreeMap<Key, Value>);
+
+fn key<T: 'static>() -> Key {
+    TypeId::of::<T>()
+}
+
+impl TypeSet {
+    /// Create an empty `TypeSet`.
+    #[must_use]
+    pub const fn new() -> Self {
+        Self(BTreeMap::new())
+    }
+
+    /// Returns true if the `TypeSet` contains zero types.
+    #[must_use]
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    /// Returns the number of distinct types in this `TypeSet`.
+    #[must_use]
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    /// Gets the corresponding type in the set for in-place manipulation.
+    ///
+    /// See [`Entry`] for usage.
+    pub fn entry<T: Send + Sync + 'static>(&mut self) -> Entry<'_, T> {
+        Entry::new(self.0.entry(key::<T>()))
+    }
+
+    /// Insert a value into this `TypeSet`.
+    ///
+    /// If a value of this type already exists, it will be replaced and returned.
+    ///
+    /// ## Example
+    /// ```rust
+    /// let mut set = typeset::TypeSet::new().with("hello");
+    /// let previous = set.insert("world");
+    /// assert_eq!(set.get::<&'static str>(), Some(&"world"));
+    /// assert_eq!(previous, Some("hello"));
+    /// ```
+    pub fn insert<T: Send + Sync + 'static>(&mut self, value: T) -> Option<T> {
+        match self.entry() {
+            Entry::Vacant(v) => {
+                log::trace!("inserting {}", std::any::type_name::<T>());
+                v.insert(value);
+                None
+            }
+
+            Entry::Occupied(mut o) => {
+                log::trace!("replacing {}", std::any::type_name::<T>());
+                Some(o.insert(value))
+            }
+        }
+    }
+
+    /// Chainable constructor to add a type to this `TypeSet`
+    ///
+    /// ## Example
+    /// ```rust
+    /// let set = typeset::TypeSet::new().with("hello");
+    /// assert_eq!(set.get::<&'static str>(), Some(&"hello"));
+    /// ```
+    #[must_use]
+    pub fn with<T: Send + Sync + 'static>(mut self, value: T) -> Self {
+        self.insert(value);
+        self
+    }
+
+    /// Check if this `TypeSet` contains a value for type T
+    ///
+    /// ## Example
+    ///
+    /// ```rust
+    /// let set = typeset::TypeSet::new().with("hello");
+    /// assert!(set.contains::<&'static str>());
+    /// assert!(!set.contains::<String>());
+    /// ```
+    #[must_use]
+    pub fn contains<T: Send + Sync + 'static>(&self) -> bool {
+        log::trace!(
+            "contains {}?: {}",
+            std::any::type_name::<T>(),
+            self.0.contains_key(&TypeId::of::<T>())
+        );
+        self.0.contains_key(&key::<T>())
+    }
+
+    /// Immutably borrow a value that has been inserted into this `TypeSet`.
+    #[must_use]
+    pub fn get<T: Send + Sync + 'static>(&self) -> Option<&T> {
+        log::trace!("getting {}", std::any::type_name::<T>(),);
+        self.0
+            .get(&key::<T>())
+            .map(|boxed| unwrap!(boxed.downcast_ref()))
+    }
+
+    /// Attempt to mutably borrow to a value that has been inserted into this `TypeSet`.
+    ///
+    /// ## Example
+    ///
+    /// ```rust
+    /// let mut set = typeset::TypeSet::new().with(String::from("hello"));
+    /// if let Some(string) = set.get_mut::<String>() {
+    ///     string.push_str(" world");
+    /// }
+    /// assert_eq!(set.get::<String>().unwrap(), "hello world");
+    /// ```
+    pub fn get_mut<T: Send + Sync + 'static>(&mut self) -> Option<&mut T> {
+        self.0
+            .get_mut(&key::<T>())
+            .map(|boxed| unwrap!(boxed.downcast_mut()))
+    }
+
+    /// Remove a value from this `TypeSet`.
+    ///
+    /// If a value of this type exists, it will be returned.
+    ///
+    /// ## Example
+    ///
+    /// ```rust
+    /// let mut set = typeset::TypeSet::new().with("hello");
+    /// assert_eq!(set.remove::<&'static str>(), Some("hello"));
+    /// assert_eq!(set.remove::<&'static str>(), None);
+    /// ```
+    pub fn remove<T: Send + Sync + 'static>(&mut self) -> Option<T> {
+        self.0
+            .remove(&key::<T>())
+            .map(|boxed| *unwrap!(boxed.downcast()))
+    }
+
+    /// Get a value from this `TypeSet` or populate it with the provided default.
+    ///
+    /// Identical to [`Entry::or_insert`]
+    ///
+    /// If building T is expensive, use [`TypeSet::get_or_insert_with`] or [`Entry::or_insert_with`]
+    ///
+    /// ## Example
+    ///
+    /// ```rust
+    /// let mut set = typeset::TypeSet::new();
+    /// assert_eq!(set.get_or_insert("hello"), &mut "hello");
+    /// assert_eq!(set.get_or_insert("world"), &mut "hello");
+    /// ```
+    pub fn get_or_insert<T: Send + Sync + 'static>(&mut self, default: T) -> &mut T {
+        self.entry().or_insert(default)
+    }
+
+    /// Get a value from this `TypeSet` or populate it with the provided default function.
+    ///
+    /// Identical to [`Entry::or_insert_with`]
+    ///
+    /// Prefer this to [`TypeSet::get_or_insert`] when building type T is expensive, since it will only be
+    /// executed when T is absent.
+    ///
+    /// ## Example
+    ///
+    /// ```rust
+    /// let mut set = typeset::TypeSet::new();
+    /// assert_eq!(set.get_or_insert_with(|| String::from("hello")), "hello");
+    /// assert_eq!(set.get_or_insert_with::<String>(|| panic!("this is never called")), "hello");
+    /// ```
+    pub fn get_or_insert_with<T: Send + Sync + 'static>(
+        &mut self,
+        default: impl FnOnce() -> T,
+    ) -> &mut T {
+        self.entry().or_insert_with(default)
+    }
+
+    /// Ensure a value is present by filling with [`Default::default`]
+    ///
+    /// Identical to [`Entry::or_default`].
+    ///
+    /// ## Example
+    ///
+    /// ```rust
+    /// let mut set = typeset::TypeSet::new().with(10usize);
+    /// let ten: usize = *set.get_or_insert_default();
+    /// assert_eq!(ten, 10);
+    /// ```
+    pub fn get_or_insert_default<T: Default + Send + Sync + 'static>(&mut self) -> &mut T {
+        self.entry().or_default()
+    }
+
+    /// Merge another `TypeSet` into this one, replacing any collisions
+    ///
+    ///
+    /// ## Example
+    ///
+    /// ```rust
+    /// let mut set_a = typeset::TypeSet::new().with(8u8).with("hello");
+    /// let set_b = typeset::TypeSet::new().with(32u32).with("world");
+    /// set_a.merge(set_b);
+    /// assert_eq!(set_a.get::<u8>(), Some(&8));
+    /// assert_eq!(set_a.get::<u32>(), Some(&32));
+    /// assert_eq!(set_a.get::<&'static str>(), Some(&"world"));
+    /// ```
+    pub fn merge(&mut self, other: TypeSet) {
+        self.0.extend(other.0);
+    }
+}
+
+impl fmt::Debug for TypeSet {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("TypeSet").finish()
+    }
+}
